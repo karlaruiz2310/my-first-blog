1ac448c78ff97bf696b995fdb4a8ff03caa0a7b4
karla <karlaadmin@uabc.edu.mx>
2020-05-24 15:52:15 -0600

    usuarios.

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,22 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('author', 'text',)
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
